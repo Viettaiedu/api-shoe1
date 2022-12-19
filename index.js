7ea0cf1933e9ae366f4f0ed0f467cd5e1c079bdd
“viettai” <viettaixca123@github.com>
2022-12-19 20:26:51 +0700

    dsa

--- index.js
+++ index.js
@@ -16,13 +16,25 @@ app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
 app.use('/public',express.static(path.join(__dirname,'/public')));
 app.use(express.json());
-app.all(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT,DELETE")
-    res.header("Access-Control-Allow-Headers", "Content-type","X-Auth-Token","Origin","Authorization","X-Requested-With");
-    res.header("Access-Control-Request-Headers: X-PINGOTHER, Content-Type")
-    next()
-  });
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
   app.use('/api/product' , routesProducts);
   app.use('/api/account',routesAccount)
   app.use('/api/cart',routesCart)
