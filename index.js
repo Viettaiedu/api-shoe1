7462582e872d8bb4918f7b9c9205103ff7f7ef66
“viettai” <viettaixca123@github.com>
2022-12-19 19:22:53 +0700

    dsa

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
 app.use('/public',express.static(path.join(__dirname,'/public')));
 app.use(express.json());
+app.use('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 app.use('/api/product' , routesProducts);
 app.use('/api/account',routesAccount)
 app.use('/api/cart',routesCart)
