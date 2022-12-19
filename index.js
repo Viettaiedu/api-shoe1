fe55df3697f54aaab7c34e74a27eb6bd5b10f631
“viettai” <viettaixca123@github.com>
2022-12-19 20:31:48 +0700

    dsa

--- index.js
+++ index.js
@@ -8,6 +8,24 @@ const routesCart = require('./routes/routesCart');
 const app = express();
 var bodyParser = require('body-parser')
 var methodOverride = require('method-override')
+// app.use(function (req, res, next) {
+
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+
+//   // Pass to next layer of middleware
+//   next();
+// });
 // override with the X-HTTP-Method-Override header in the request
 app.use(methodOverride('_method'))
 // parse application/x-www-form-urlencoded
@@ -16,24 +34,7 @@ app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
 app.use('/public',express.static(path.join(__dirname,'/public')));
 app.use(express.json());
-app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 
   app.use('/api/product' , routesProducts);
   app.use('/api/account',routesAccount)
