f8b08d0335e8da41dde57b1b074f25a26e5de63c
“viettai” <viettaixca123@github.com>
2022-12-20 16:01:01 +0700

    das

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -14,10 +14,10 @@ exports.addCart = async (req,res,next) => {
     if(!cartDatabase[0]) {
         const cart = new Cart(product[0].id,product[0].gender,product[0].size,product[0].price,product[0].star,product[0].name,product[0].image,product[0].discount,product[0].trademark,product[0].state,newQty);
         await cart.save();
-        return res.redirect('https://superlative-crepe-8d38c1.netlify.app/cart')
+        return res.location('https://superlative-crepe-8d38c1.netlify.app/cart')
     }else if(newQty !== cartDatabase[0].qty) {
         await Cart.update(parseInt(cartDatabase[0].id),newQty);
-        return res.redirect('https://superlative-crepe-8d38c1.netlify.app/cart')
+        return res.location('https://superlative-crepe-8d38c1.netlify.app/cart')
     }
     return res.status(404).send({
         message :"Error server"
@@ -30,7 +30,7 @@ exports.removeProductFromCart = async (req,res,next) => {
     const [cartDatabase,____] = await Cart.findById(parseInt(id ));
     if(cartDatabase) {
         await Cart.delete(parseInt(id ));
-        return res.redirect('https://superlative-crepe-8d38c1.netlify.app/cart')
+        return res.location('https://superlative-crepe-8d38c1.netlify.app/cart')
         return  res.status(200).json({
             redirect:"/cart"
         });
