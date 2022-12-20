bb2b4887c5458923514300401c1373435e36039c
“viettai” <viettaixca123@github.com>
2022-12-20 15:40:59 +0700

    ds

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -14,12 +14,16 @@ exports.addCart = async (req,res,next) => {
     if(!cartDatabase[0]) {
         const cart = new Cart(product[0].id,product[0].gender,product[0].size,product[0].price,product[0].star,product[0].name,product[0].image,product[0].discount,product[0].trademark,product[0].state,newQty);
         await cart.save();
+        return res.status(201).json({
+            redirect :"/cart"
+        })
     }else if(newQty !== cartDatabase[0].qty) {
         await Cart.update(parseInt(cartDatabase[0].id),newQty);
         return res.status(201).json({
             redirect :"/cart"
         })
     }
+    
     return res.status(404).send({
         message :"Error server"
     })
@@ -32,9 +36,7 @@ exports.addCart = async (req,res,next) => {
 exports.removeProductFromCart = async (req,res,next) => {
     const { id } = req.params;
     var idDelete = parseInt(id);
-    console.log(idDelete)
     const [cartDatabase,____] = await Cart.findById(parseInt(idDelete));
-    console.log(cartDatabase)
     if(cartDatabase) {
         await Cart.delete(idDelete);
         return  res.status(200).json({
