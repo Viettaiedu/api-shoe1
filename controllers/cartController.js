8c9529d15435e328684da5f684f778465d2b7c34
“viettai” <viettaixca123@github.com>
2022-12-20 10:54:16 +0700

    ds

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -7,29 +7,30 @@ exports.getCart = async (req,res,next) => {
 }
 exports.addCart = async (req,res,next) => {
     const {id ,qty} = req.body;
-    var newId = parseInt(id)
-    var newQty = parseInt(qty);
-    var [product , _] = await Product.findById(newId);
-    const [cartDatabase,____] = await Cart.findById(parseInt(id));
-    if(!cartDatabase[0]) {
-        const cart = new Cart(product[0].id,product[0].gender,product[0].size,product[0].price,product[0].star,product[0].name,product[0].image,product[0].discount,product[0].trademark,product[0].state,newQty);
-        await cart.save();
-    }else if(newQty !== cartDatabase[0].qty) {
-        await Cart.update(parseInt(cartDatabase[0].id),newQty);
-    }
-    res.redirect('/cart');
+    // var newId = parseInt(id)
+    // var newQty = parseInt(qty);
+    // var [product , _] = await Product.findById(newId);
+    // const [cartDatabase,____] = await Cart.findById(parseInt(id));
+    // if(!cartDatabase[0]) {
+    //     const cart = new Cart(product[0].id,product[0].gender,product[0].size,product[0].price,product[0].star,product[0].name,product[0].image,product[0].discount,product[0].trademark,product[0].state,newQty);
+    //     await cart.save();
+    // }else if(newQty !== cartDatabase[0].qty) {
+    //     await Cart.update(parseInt(cartDatabase[0].id),newQty);
+    // }
+    res.json(req.body);
 }
 
 
 exports.removeProductFromCart = async (req,res,next) => {
-    const {id} = req.body;
+    const { id } = req.params;
     var idDelete = parseInt(id);
-    const [cartDatabase,____] = await Cart.findById(parseInt(id));
+    console.log(idDelete)
+    const [cartDatabase,____] = await Cart.findById(parseInt(idDelete));
+    console.log(cartDatabase)
     if(cartDatabase) {
         await Cart.delete(idDelete);
       return  res.redirect("/cart");
     }
-
     return  res.json("ERROR");
     
 }
